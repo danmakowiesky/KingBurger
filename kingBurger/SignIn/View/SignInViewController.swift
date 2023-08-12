f8223c3aeac89503fad97e78607d65e446e9d38a
danmakowiesky <dvm1993@hotmail.com>
2023-08-11 21:55:16 -0300

    adicionando input password

--- kingBurger/SignIn/View/SignInViewController.swift
+++ kingBurger/SignIn/View/SignInViewController.swift
@@ -11,16 +11,39 @@ import UIKit
 class SignInViewController: UIViewController {
     let email: UITextField = {
         let emailInput = UITextField()
+        emailInput.translatesAutoresizingMaskIntoConstraints = false
         emailInput.backgroundColor = UIColor.white
         emailInput.placeholder = "Entre com seu e-mail"
-        emailInput.placeholder = "Entre com seu e-mail"
         return emailInput
+    }()
+    let password: UITextField = {
+        let passwordInput = UITextField()
+        passwordInput.translatesAutoresizingMaskIntoConstraints = false
+        passwordInput.backgroundColor = UIColor.white
+        passwordInput.placeholder = "Digite sua senha"
+        return passwordInput
     }()
    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.cyan
         view.addSubview(email)
-        email.frame = CGRect(x:0, y: view.bounds.size.height / 2, width: view.bounds.size.height, height: 50)
+        let emailConstraints = [
+            email.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            email.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            email.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100.0),
+            email.heightAnchor.constraint(equalToConstant: 50.0)
+
+        ]
+        view.addSubview(password)
+        let passwordConstraints = [
+            password.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            password.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            password.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 10.0),
+            password.heightAnchor.constraint(equalToConstant: 50.0)
+        ]
+        NSLayoutConstraint.activate(emailConstraints)
+        NSLayoutConstraint.activate(passwordConstraints)
+
     }
 }
